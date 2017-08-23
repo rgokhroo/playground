d32876fa2ac80a9cc0266144474bacd77131c728
rgokhroo <ranu_gokhroo@intuit.com>
2017-08-22 23:52:33 -0700

    Initial project setup

---
+++ invoiceService.js
@@ -0,0 +1,13 @@
+// === HTTP service ===
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+// maps contents of /static folder to the site's root
+app.use(express.static(__dirname + '/static/html')); 
+console.log(__dirname);
+
+// start listening 
+server.listen(5000, function () {
+  console.log('Example JS service is listening on http://localhost:5000');
+});
